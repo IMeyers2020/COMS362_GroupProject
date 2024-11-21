aea9418d646d30605c2975a8ed3a572db9ca9af0
Isaac Meyers <IMeyers701@gmail.com>
2024-11-20 19:30:02 -0600

    feat: one mroe

--- app/models/academics/RegistrationController.java
+++ app/models/academics/RegistrationController.java
@@ -3,8 +3,16 @@ import java.util.HashMap;
 
 import models.academics.items.Registration;
 import models.general.items.Course;
+import src.DatabaseSupport;
 
 public class RegistrationController {
+
+    DatabaseSupport db;
+
+    public RegistrationController(DatabaseSupport _db) {
+        this.db = _db;
+    }
+
     public boolean addCourse(Course cid, String sid, Integer credHours) {
         Registration r = new Registration();
         return r.addCourse(cid, sid, credHours);
