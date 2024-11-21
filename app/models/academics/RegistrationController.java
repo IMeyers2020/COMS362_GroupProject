1692d477b5b67afd6671f1c4aaf70ee2e5b283a7
Isaac Meyers <IMeyers701@gmail.com>
2024-11-20 19:39:11 -0600

    feat: add a couple mroe fixes

--- app/models/academics/RegistrationController.java
+++ app/models/academics/RegistrationController.java
@@ -1,8 +1,10 @@
 package models.academics;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 import models.academics.items.Registration;
 import models.general.items.Course;
+import models.general.items.courseLookup;
+import models.general.people.studentLookup;
 import src.DatabaseSupport;
 
 public class RegistrationController {
@@ -15,15 +17,22 @@ public class RegistrationController {
 
     public boolean addCourse(Course cid, String sid, Integer credHours) {
         Registration r = new Registration();
-        return r.addCourse(cid, sid, credHours);
+        studentLookup stud = this.db.getStudent(sid);
+        stud.value.addCourse(cid);
+        this.db.updateStudent(sid, stud.value);
+        return true;
     }
 
     public boolean removeCourse(String sid, Course c) {
         Registration r = new Registration();
-        return r.removeCourse(sid, c);
+        studentLookup stud = this.db.getStudent(sid);
+        stud.value.removeCourse(c);
+        this.db.updateStudent(sid, stud.value);
+        return true;
     }
 
-    public HashMap<String, Course> viewRegisteredCourses(String sid) {
-        return (new Registration()).getRegisteredCoursesForStudent(sid);
+    public ArrayList<courseLookup> viewRegisteredCourses(String sid) {
+        studentLookup stud = this.db.getStudent(sid);
+        return stud.value.getCurrentCourses();
     }
 }
--- app/models/academics/items/Registration.java
+++ app/models/academics/items/Registration.java
@@ -6,29 +6,7 @@ import models.general.items.Course;
 import src.DatabaseSupport;
 
 public class Registration {
-    //subject to change once saved files have been set up
-    public boolean addCourse(Course c, String sid, Integer credHours) {
-        // Course c = new Course(cid, credHours);
-        DatabaseSupport ds = new DatabaseSupport();
-        //will be replaced by 'student s = ds.getStudent(sid)' if (s.addCourse(c)) {return ds.putStudent(s)}
-        if(ds.getStudent(sid).addCourse(c)) {
-            return true;
-        }
-        return false;
-    }
-
-    //subject to change once saved files have been set up
-    public boolean removeCourse(String sid, Course c) {
-        DatabaseSupport ds = new DatabaseSupport();
-        
-        //will be replaced by 'student s = ds.getStudent(sid)' if (s.removeCourse(c)) {return ds.putStudent(s)}
-        if(ds.getStudent(sid).removeCourse(c)) {
-            return true;
-        }
-        return false;
-    }
-
-    public HashMap<String, Course> getRegisteredCoursesForStudent(String sid) {
-        return DatabaseSupport.getRegisteredCoursesForStudent(sid);
-    }
+    // public HashMap<String, Course> getRegisteredCoursesForStudent(String sid) {
+    //     return DatabaseSupport.getRegisteredCoursesForStudent(sid);
+    // }
 }
